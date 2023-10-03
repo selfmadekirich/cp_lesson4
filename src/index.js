fcc8b2a0f9e0552adcca34071462de2fbb812636
selfmadekirich <tosha.kirichenko.2001@mail.ru>
2023-10-04 02:09:10 +0300

    add ui

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
+import {MiniMaple} from "../src/miniMaple";
+import { InvalidOperationError } from "../src/Errors/CustomErrors";
+import { InvalidArgumentError } from "../src/Errors/CustomErrors";
+
 document.addEventListener('DOMContentLoaded',setup)
 
 function setup() {
-    document.getElementById('demoButton').onclick = addSomething;
+    document.getElementById('demoButton').onclick = CalculateDerivative;
 }
 
-function addSomething(){
-    const someDummyDiv = document.createElement('div');
-    someDummyDiv.classList.add('generated');
-    const count = document.getElementsByClassName('generated').length;
-    someDummyDiv.innerHTML = `I was created by JS! There are already ${count} of my friends!`;
-    const container = document.getElementById('container');
-    container.appendChild(someDummyDiv);
+function Validate(params){
+    
+    for(const param of params){
+        if(!param){
+            alert("All fields must be filled")
+        }
+    }
+}
+ 
+function CalculateDerivative(){
+    const input_formula = document.getElementById('input_formula').value;
+    const input_var = document.getElementById('input_var').value;
+    const Result = document.getElementById('Result');
+    Validate([input_formula,input_var])
+    try{
+        const mm = new MiniMaple().diff(input_formula,input_var)
+        Result.innerHTML= "Result:" + mm
+    }
+    catch(e){
+        alert(e)
+    }
+
 }
